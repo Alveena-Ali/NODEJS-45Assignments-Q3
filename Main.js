acda846a00ca01875a619f8610bb1f3d6ff0d1fc
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-06 22:08:36 +0500

    Que 3: Name Cases

---
+++ Main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Q3: Name Cases: Store a person's name in a varible, and then print that person's name in lowercase, 
+// uppercase, and  titlecase.
+let firstName = "alveena ali";
+console.log(firstName.toLocaleLowerCase());
+console.log(firstName.toLocaleUpperCase());
+console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1));
